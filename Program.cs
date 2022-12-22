8510fa95ccff17d4b2a7302eb8de6836069d13d1
ZaidEmara <121249518+ZaidEmara@users.noreply.github.com>
2022-12-22 22:46:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace PERFECT_NUMBER
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int start, end, factor, i, sum;
+
+            Console.WriteLine("start and end of range:");
+
+            Console.WriteLine("Enter start");
+            start = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter end");
+            end = int.Parse(Console.ReadLine());
+            Console.WriteLine("perfect numbers are ");
+
+            for (i = start; i <= end; i++)
+            {
+
+                factor = 1;
+                sum = 0;
+                while (factor < i)
+                {
+                    if (i % factor == 0)
+
+                        sum += factor;
+                    factor++;
+                }
+                if (sum == i)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
